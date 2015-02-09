2cd89cb711ef38e83f936be29c07b150e5ff6d76
James Hotchkin <jhotch@users.noreply.github.com>
2015-02-09 00:40:50 -0600

    adding clojure-mode and cider

--- init.el
+++ init.el
@@ -61,6 +61,24 @@
 ;; Load the enabled modules.
 (when (not (boundp 'ohai/wizard-did-run)) (ohai/load-modules))
 
-;; activate rainbow-delimiters-mode in most programming modes
+;; install clojure mode and extra syntax highlighting
+(unless (package-installed-p 'clojure-mode)
+  (package-install 'clojure-mode))
+(require 'clojure-mode)
+
+(unless (package-installed-p 'clojure-mode-extra-font-locking)
+  (package-install 'clojure-mode-extra-font-locking))
+(require 'clojure-mode-extra-font-locking)
+
+;; install CIDER package from stable MELPA repo
+(add-to-list 'package-pinned-packages '(cider . "melpa-stable") t)
+(unless (package-installed-p 'cider)
+  (package-install 'cider))
+(require 'cider)
+
+;; install and activate rainbow-delimiters-mode
+;; in most programming modes
+(unless (package-installed-p 'rainbow-delimiters)
+  (package-install 'rainbow-delimiters))
 (require 'rainbow-delimiters)
 (add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
