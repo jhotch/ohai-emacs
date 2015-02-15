2b9d4ca9fc7124ef47d86639a87ede9ffdcfffa8
James Hotchkin <jhotch@users.noreply.github.com>
2015-02-14 21:59:20 -0600

    moving customizations to user.el

--- init.el
+++ init.el
@@ -73,32 +73,5 @@ Your installed Emacs reports:
 ;; Load the enabled modules.
 (when (not (boundp 'ohai/wizard-did-run)) (ohai/load-modules))
 
-;; install dart mode
-(unless (package-installed-p 'dart-mode)
-  (package-install 'dart-mode))
-(require 'dart-mode)
-
-;; install clojure mode and extra syntax highlighting
-(unless (package-installed-p 'clojure-mode)
-  (package-install 'clojure-mode))
-(require 'clojure-mode)
-
-(unless (package-installed-p 'clojure-mode-extra-font-locking)
-  (package-install 'clojure-mode-extra-font-locking))
-(require 'clojure-mode-extra-font-locking)
-
-;; install CIDER package from stable MELPA repo
-(add-to-list 'package-pinned-packages '(cider . "melpa-stable") t)
-(unless (package-installed-p 'cider)
-  (package-install 'cider))
-(require 'cider)
-
-;; install and activate rainbow-delimiters-mode
-;; in most programming modes
-(unless (package-installed-p 'rainbow-delimiters)
-  (package-install 'rainbow-delimiters))
-(require 'rainbow-delimiters)
-(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
-
 ;; Load the user's config, if it exists.
 (load (concat dotfiles-dir "user.el") 'noerror)
